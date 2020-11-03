801036198a5d4f5e95805f4ec11e4b0fe3cba207
will <wye139629@gmail.com>
2020-11-03 16:23:48 +0800

    set rails time zone to taipei

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module ProjectManagement
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = "zh-TW"
+    config.time_zone = "Taipei"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
