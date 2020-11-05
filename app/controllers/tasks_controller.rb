c5f5a7452e87fa8ddea70d7d4752641c5e161ee2
will <wye139629@gmail.com>
2020-11-05 11:04:56 +0800

    add start and end date to index/form view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,7 +39,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :start_date, :end_date)
   end
 
   def find_task
